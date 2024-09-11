5ab771b0d78c42d333ee95f2017cf8041fa952be
rzkihdyh <rizkihidayatul22@gmail.com>
2024-09-11 14:32:59 +0700

    menyelesaikan tutorial2 pt2

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from main.views import show_main
 from main.views import show_main, create_mood_entry
+from main.views import show_main
 from main.views import show_main, create_mood_entry, show_xml
 from main.views import show_main, create_mood_entry, show_xml, show_json
 from main.views import show_main, create_mood_entry, show_xml, show_json, show_xml_by_id, show_json_by_id
